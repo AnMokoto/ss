950a31e351aeaa0162994799c94707436f544563
AnMokoto <chinaume@163.com>
2019-03-21 11:22:37 +0800

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,27 @@
 #!/bin/bash -ex
 
-yum install python-setuptools && easy_install pip
-pip install shadowsocks
-yum install m2crypto
+pip install --upgrade pip
+
+centos(){
+  yum install python-setuptools && easy_install pip
+  pip install shadowsocks
+  yum install m2crypto
+}
+
+ubuntu(){
+  apt-get install python-setuptools && easy_install pip
+  pip install shadowsocks
+  apt-get install m2crypto
+}
+
+if [ -f "/etc/redhat-release" ]; then
+		centos
+	else
+		# /etc/lsb-release
+		ubuntu
+	fi	
+
+
 
 sleep 1
 bash -x ./shadowsocks.sh
